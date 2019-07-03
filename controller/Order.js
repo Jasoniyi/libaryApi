d4df11748ff4e246fa1bd295bd19fce1bd81b6ea
Jasoniyi <josephomoniyitope@gmail.com>
2019-07-03 20:49:31 +0100

    feat(admin)get all records[Deleivers #166924900]

--- controller/Order.js
+++ controller/Order.js
@@ -26,13 +26,13 @@ const Order = {
     } catch (err) {
       return res.status(400).send(err);
     }
-  }
+  },
   /** return book
    *@params {object} req
    *@params {object} res
-    *@returns {object} book object
+   *@returns {object} book object
    */
-  async return(req,res) {
+  async return(req, res) {
     const returnQuery = `INSERT INTO orders(order_id, order_on, type, book, due_date, owner_id)
     VALUES ($1, $2, $3, $4, $5, $6) returning *`;
     const values = [
@@ -47,8 +47,23 @@ const Order = {
     try {
       const { rows } = await db.query(returnQuery, values);
       return res.status(200).send(rows[0]);
-    } catch(err) {
-      return res.status(400).send(err)
+    } catch (err) {
+      return res.status(400).send(err);
+    }
+  },
+  /**
+   * get all records
+   * @params {object} req
+   * @params {object} res
+   * @returns {object} all records
+   */
+  async getAll(req, res) {
+    const findAll = 'SELECT * FROM orders WHERE owner_id = $1';
+    try {
+      const { rows, rowCount } = await db.query(findAll, [req.user.id]);
+      return res.status(200).send({ rows, rowCount });
+    } catch (err) {
+      return res.status(400).send(err);
     }
   }
 };
